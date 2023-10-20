2a34bedb8048c7b426e7e920b71bb901d99a6464
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-10-20 11:05:43 +0200

    Refresh

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -18,8 +18,7 @@
 <body>
     <nav class="top-nav">
         <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
+            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a></div>
         <div class="top-nav-links">
             <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentation</a>
             <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
