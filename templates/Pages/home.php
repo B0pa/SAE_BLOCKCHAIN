548d302f65950cb563a280ecdb9621ad73e85615
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-10-24 17:38:23 +0200

    footer.php

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -82,9 +82,7 @@
 </div>
 
 
-<footer class="bg-dark text-white text-center mt-4">
-    <p class="mb-0">Tous droit réservé</p>
-</footer>
+<?= $this->element('footer')?>
 
 </body>
 </html>
---
+++ templates/element/footer.php
@@ -0,0 +1,4 @@
+<footer class="bg-dark text-white text-center mt-4">
+    <p class="mb-0">Tous droit réservé</p>
+</footer>
+
