f6e80921d904cbc25b2f00eefeab798f69025e74
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-10-26 09:12:25 +0200

    nav et css

--- templates/Pages/tempreel.php
+++ templates/Pages/tempreel.php
@@ -7,17 +7,10 @@
     <meta charset="UTF-8">
     <title>Temp réel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<nav>
-    <a href="/pages/home"><img alt="maison"></a>
-
-    <div class="titrejaune">
-        <h1>Temps Réel</h1>
-    </div>
-
-</nav>
+<?= $this->element('nav2')?>
 <main>
 
     <div>
--- templates/Pages/wallet.php
+++ templates/Pages/wallet.php
@@ -7,34 +7,26 @@
     <meta charset="UTF-8">
     <title>Wallet</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<nav>
-    <a href="/pages/home"><?= $this->Html->image('acceuil.png', ['alt' => 'acceuil']); ?></a>
-
-
-
-    <div class="titrejaune">
-        <h1>Wallet</h1>
-    </div>
-</nav>
+<?= $this->element('nav2')?>
 
 <main>
     <div>
-        <?= $this->Html->image('cryptobitcoin.png', ['alt' => 'crypto Bitcoin']); ?>
+        <?= $this->Html->image('cryptobitcoin.png', ['class' => 'rounded-circle','alt' => 'crypto Bitcoin']); ?>
         <p>0</p>
     </div>
     <div>
-        <?= $this->Html->image('cryptoblockchain.png', ['alt' => 'crypto Blockchain']); ?>
+        <?= $this->Html->image('cryptoblockchain.png', ['class' => 'rounded-circle','alt' => 'crypto Blockchain']); ?>
         <p>0</p>
     </div>
     <div>
-        <?= $this->Html->image('cryptodanger.png', ['alt' => 'crypto Danger']); ?>
+        <?= $this->Html->image('cryptodanger.png', ['class' => 'rounded-circle','alt' => 'crypto Danger']); ?>
         <p>0</p>
     </div>
     <div>
-        <?= $this->Html->image('cryptoNFT.png', ['alt' => 'crypto NFT']); ?>
+        <?= $this->Html->image('cryptoNFT.png', ['class' => 'rounded-circle','alt' => 'crypto NFT']); ?>
         <p>0</p>
     </div>
 
