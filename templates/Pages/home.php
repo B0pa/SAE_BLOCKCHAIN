b2fcdea525b7569ffa75a3d728f686a474cf7712
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2023-10-26 11:00:50 +0200

    Home responsive

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -11,21 +11,21 @@
 <body class="bg-secondary">
 
     <header>
-        <nav class="navbar navbar-dark bg-dark d-flex fixed-top" style="height: 100px;">
+        <nav class="navbar navbar-dark bg-dark d-flex fixed-top" style="height: 105px;">
             <a href="/pages/wallet" class="nav-link d-flex align-items-center"><?= $this->Html->image('wallet.png', ['class' => 'img-fluid','alt' => 'icone wallet']); ?></a>
             <a  href="/pages/tempreel" class="nav-link d-flex align-items-center"><?= $this->Html->image('temp reel.png', ['class' => 'img-fluid h-100','alt' => 'icone temp réel']); ?></a>
 
-            <div class="bg-warning rounded-pill col-3 mx-auto align-self-center">
-                <h1 class="h1 text-center ">Accueil</h1>
+            <div class="d-flex bg-warning rounded-pill col-4 col-md-3 mx-auto align-items-center justify-content-center h-auto p-1">
+                <h1 class="h1 text-center  ">Accueil</h1>
             </div>
 
         </nav>
     </header>
-<div class="d-flex mx-5" >
-    <main class="flex-grow-1 flex-shrink-0" style="padding-top: 125px" >
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-11 d-flex">
+<div class="d-flex flex-column flex-md-row mx-1 mt-5 pt-5 mx-md-5" >
+    <main class="flex-grow-1 flex-shrink-0">
+            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex">
                 
-                    <?= $this->Html->image('NFT.png', ['alt' => 'icone NFT', 'style' => 'height:100px' , 'class' => 'img-thumbnail']); ?>
+                    <?= $this->Html->image('NFT.png', ['alt' => 'icone NFT', 'style' => 'height:100px' , 'class' => 'img-thumbnail img-fluid']); ?>
                 
                 
 
@@ -35,7 +35,7 @@
                 </div>
             </div>
 
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-11 d-flex">
+            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex">
 
                 <?= $this->Html->image('crypto.png', ['alt' => 'icone cryptomonnais', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
 
@@ -45,7 +45,7 @@
                 </div>
             </div>
 
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-11 d-flex" >
+            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex" >
 
                 <?= $this->Html->image('danger.png', ['alt' => 'icone danger', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
 
@@ -55,7 +55,7 @@
                 </div>
             </div>
 
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-11 d-flex" >
+            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex" >
                 
 
                 <?= $this->Html->image('blockchain.jpg', ['alt' => 'blockchain', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
@@ -65,12 +65,12 @@
                 </div>
             </div>
 
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-11">
+            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11">
                 <h2 class="h2 text-center" >Qui sommes-nous ?</h2>
             </div>
     </main>
 
-    <aside class="flex-shrink-0 text-white bg-dark rounded col-3 p-3" style="margin-top:150px" >
+    <aside class="home-aside flex-shrink-0 text-white bg-dark rounded col-0 col-md-4 p-3 mt-4">
         <div class="text-center" >
             <h2 class="h2">Les actualités</h2>
         </div>
