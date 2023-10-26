dbb00aa7a43cd863418e76b8b7e3e744c25f50aa
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-10-26 11:16:38 +0200

    home page thomas

--- templates/Pages/wallet.php
+++ templates/Pages/wallet.php
@@ -10,8 +10,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<?= $this->element('nav2')?>
-
+<header>
+    <?= $this->element('nav2')?>
+</header>
 <main>
     <div>
         <?= $this->Html->image('cryptobitcoin.png', ['class' => 'rounded-circle','alt' => 'crypto Bitcoin']); ?>
