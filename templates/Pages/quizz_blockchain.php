56251a8b000000380b039953543f763efa9b7ded
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-10-27 16:54:15 +0200

    Quiz NFT

--- templates/Pages/quizz_blockchain.php
+++ templates/Pages/quizz_blockchain.php
@@ -1,15 +1,39 @@
-<?php
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Quizz Blockchain</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz sur la Blockchain</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body class="bg-secondary">
-<?= $this->element('nav')?>
 
+</head>
+<body>
+<nav>
+
+</nav>
+<main>
+    <div id="quiz-container">
+        <div class="PTop">
+            <h1>Quiz sur la Blockchain</h1>
+            <p id="score"></p>
+        </div>
+        <div class="PTop">
+            <p id="question">Question 1 : Qu'est-ce que la blockchain ?</p>
+            <p id="difficulty">Easy</p>
+        </div>
+        <div id="options" class="grid-container">
+            <input type="radio" name="choice" id="choice1" value="A">
+            <label for="choice1"></label>
+            <input type="radio" name="choice" id="choice2" value="B">
+            <label for="choice2">=</label>
+            <input type="radio" name="choice" id="choice3" value="C">
+            <label for="choice3"></label>
+        </div>
+        <button id="submit-button" onclick="checkAnswer()">Soumettre</button>
+    </div>
+    <div id="result-container">
+        <p id="result"></p>
+    </div>
+    <script src="/JS/quizBChain.js"></script>
+</main>
 </body>
+</html>
--- templates/Pages/quizzcrypto.php
+++ templates/Pages/quizzcrypto.php
@@ -1,15 +1,38 @@
-<?php
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Quizz Cryptomonnaies</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz sur la Blockchain</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body class="bg-secondary">
-<?= $this->element('nav')?>
+<body>
+<nav>
 
+</nav>
+<main>
+    <div id="quiz-container">
+        <div class="PTop">
+            <h1>Quiz sur la Blockchain</h1>
+            <p id="score"></p>
+        </div>
+        <div class="PTop">
+            <p id="question">Question 1 : Qu'est-ce que la cryptomonnaie ?</p>
+            <p id="difficulty">Easy</p>
+        </div>
+        <div id="options" class="grid-container">
+            <input type="radio" name="choice" id="choice1" value="A">
+            <label for="choice1">A. Une monnaies décentralisée.</label><br>
+            <input type="radio" name="choice" id="choice2" value="B">
+            <label for="choice2">B. Un objet que l'ont trouve en magasins.</label><br>
+            <input type="radio" name="choice" id="choice3" value="C">
+            <label for="choice3">C. Une chanson populaire de Barack Obama.</label><br>
+        </div>
+        <button id="submit-button" onclick="checkAnswer()">Soumettre</button>
+    </div>
+    <div id="result-container">
+        <p id="result"></p>
+    </div>
+    <script src="quiz.js"></script>
+</main>
 </body>
+</html>
