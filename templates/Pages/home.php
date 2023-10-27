6e1e6292e0c9db533b74323561c5723f9bf33727
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2023-10-27 15:18:32 +0200

    h

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,127 +1,69 @@
 <!DOCTYPE html>
-<html lang="en-FR">
-<html lang="en-FR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>ACCUEIL</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
+<body class="bg-secondary" >
+<nav class="bg-dark d-flex">
+    <a><img alt="wallet"></a>
+    <a><img alt="temp réel"></a>
 
-<body class="bg-secondary">
+    <div class="bg-warning rounded-pill col-5 mx-auto">
+        <h1 class="h1 text-center">Accueil</h1>
+    </div>
 
-    <header>
-        <nav class="navbar navbar-dark bg-dark d-flex fixed-top" style="height: 105px;">
-            <a href="/pages/wallet" class="nav-link d-flex align-items-center"><?= $this->Html->image('wallet.png', ['class' => 'img-fluid','alt' => 'icone wallet']); ?></a>
-            <a  href="/pages/tempreel" class="nav-link d-flex align-items-center"><?= $this->Html->image('temp reel.png', ['class' => 'img-fluid h-100','alt' => 'icone temp réel']); ?></a>
-    <header>
-        <nav class="navbar navbar-dark bg-dark d-flex fixed-top" style="height: 105px;">
-            <a href="/pages/wallet" class="nav-link d-flex align-items-center"><?= $this->Html->image('wallet.png', ['class' => 'img-fluid','alt' => 'icone wallet']); ?></a>
-            <a  href="/pages/tempreel" class="nav-link d-flex align-items-center"><?= $this->Html->image('temp reel.png', ['class' => 'img-fluid h-100','alt' => 'icone temp réel']); ?></a>
 
-            <div class="d-flex bg-warning rounded-pill col-4 col-md-3 mx-auto align-items-center justify-content-center h-auto p-1">
-                <h1 class="h1 text-center  ">Accueil</h1>
-            </div>
-            <div class="d-flex bg-warning rounded-pill col-4 col-md-3 mx-auto align-items-center justify-content-center h-auto p-1">
-                <h1 class="h1 text-center  ">Accueil</h1>
-            </div>
 
-<<<<<<<<< Temporary merge branch 1
-        <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex">
 
-                <?= $this->Html->image('crypto.png', ['alt' => 'icone cryptomonnais', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-
-            <h2 class="h2 mx-auto">Les Crypto</h2>
-            <div class="btn btn-secondary align-self-end" >
-                <a href="/pages/crypto" class="text-white text-decoration-none" >Explications</a>
-=========
-        </nav>
-    </header>
-<div class="d-flex flex-column flex-md-row mx-1 mt-5 pt-5 mx-md-5" >
-    <main class="flex-grow-1 flex-shrink-0">
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex">
-                
-                    <?= $this->Html->image('NFT.png', ['alt' => 'icone NFT', 'style' => 'height:100px' , 'class' => 'img-thumbnail img-fluid']); ?>
-                
-                
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex">
-                
-                    <?= $this->Html->image('NFT.png', ['alt' => 'icone NFT', 'style' => 'height:100px' , 'class' => 'img-thumbnail img-fluid']); ?>
-                
-                
-
-                <h2 class="h2 mx-auto">Les NFT</h2>
-                <div class="btn btn-secondary align-self-end" >
-                    <a href="/pages/explication" class="text-white text-decoration-none" >Explications</a>
-                </div>
-            </div>
-            <h2 class="h2 mx-auto">Les NFT</h2>
-            <div class="btn btn-secondary align-self-end" >
-                <a href="/pages/NFT" class="text-white text-decoration-none" >Explications</a>
+</nav>
+<div CLASS="separateur"></div>
+<div class="contenu">
+    <main>
+        <div class="explication">
+            <img src='/webroot/img/NFT.png' alt='icone NFT'/>
+            <h2 class="h2" >Les NFT</h2>
+            <div>
+                <a>Explication</a>
             </div>
         </div>
 
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex">
-
-                <?= $this->Html->image('crypto.png', ['alt' => 'icone cryptomonnais', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-
-            <h2 class="h2 mx-auto">Les Crypto</h2>
-            <div class="btn btn-secondary align-self-end" >
-                <a href="/pages/crypto" class="text-white text-decoration-none" >Explications</a>
+        <div class="explication">
+            <img src="/webroot/img/crypto.png" alt="icone cryptomonnaies"/>
+            <h2 class="h2" >Les Crypto</h2>
+            <div>
+                <a>Explication</a>
             </div>
         </div>
 
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex" >
-
-                <?= $this->Html->image('danger.png', ['alt' => 'icone danger', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-                <?= $this->Html->image('danger.png', ['alt' => 'icone danger', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-
-            <h2 class="h2 mx-auto" >Les dangers</h2>
-            <div class="btn btn-secondary align-self-end" >
-                <a href="/pages/danger" class="text-white text-decoration-none" >Explications</a>
-            </div>
-        </div>
+        <div class="explication" >
+            <img src="/webroot/img/danger.png" alt="icone danger"/>
+            <h2 class="h2" >Les dangers</h2>
+            <div>
+                <a>Explication</a>
             </div>
         </div>
 
-            <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex" >
-                
-
-            <?= $this->Html->image('blockchain.jpg', ['alt' => 'blockchain', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-            <h2 class="h2 mx-auto">Les blockchain</h2>
-            <div class="btn btn-secondary align-self-end" >
-                <a href="/pages/blockchain" class="text-white text-decoration-none" >Explications</a>
-            </div>
-        </div>
+        <div class="explication" >
+            <h2 class="h2" >Les blockchain</h2>
+            <img src="/webroot/img/blockchain.jpg" alt="icone blockchain"/>
+            <div>
+                <a>Explication</a>
             </div>
         </div>
 
-        <div class="explication p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11">
-            <h2 class="h2 text-center" >Qui sommes-nous ?</h2>
+        <div class="explication">
+            <h2 class="h2" >Qui sommes-nous ?</h2>
         </div>
     </main>
 
-    <aside class="home-aside flex-shrink-0 text-white bg-dark rounded col-0 col-md-4 p-3 mt-4">
-    <aside class="home-aside flex-shrink-0 text-white bg-dark rounded col-0 col-md-4 p-3 mt-4">
-    <aside class="home-aside flex-shrink-0 text-white bg-dark rounded col-0 col-md-4 p-3 mt-4">
-        <div class="text-center" >
-            <h2 class="h2">Les actualités</h2>
-        </div>
-        <div class="bg-secondary rounded-2 p-2" >
-            <h3 class="h3 text-center">blabla</h3>
-            <p class="text-center" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident veniam nihil nemo asperiores? Possimus rem quod officia similique eaque laudantium aperiam, doloremque molestias magni earum vel quis dolorem natus!</p>
-        </div>
-
-        <div class="btn btn-secondary align-self-end " >
-            <a href="/pages/actualite" class="text-white text-decoration-none" >Actualité</a>
+    <aside>
+        <div>
+            <h2 class="h2" >Les actualités</h2>
         </div>
     </aside>
 </div>
-    
-    
-    <footer class="bg-dark text-white text-center mt-4">
-        <p class="mb-0">Tous droit réservé</p>
-    </footer>
-
 </body>
 </html>
