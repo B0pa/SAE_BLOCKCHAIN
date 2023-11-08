e63500bd6c715350497d358e4835f361f2343e8b
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-11-08 08:54:59 +0100

    Nav 3

--- templates/element/nav3.php
+++ templates/element/nav3.php
@@ -1,20 +1,21 @@
 <?php
+
 $currentURL = strtolower(basename($_SERVER['REQUEST_URI']));
 $pageTitle = 'Erreur'; // Valeur par défaut
+$crypto = '';
 
-// Définissez les titres de page en fonction de l'URL
-if ($currentURL === 'explication') {
-    $pageTitle = 'Explication';
-} elseif ($currentURL === 'actualite') {
-    $pageTitle = 'Actualité';
-} elseif ($currentURL === 'nft') {
-    $pageTitle = 'NFT';
-} elseif ($currentURL === 'danger') {
-    $pageTitle = 'Danger';
-} elseif ($currentURL === 'crypto') {
-    $pageTitle = 'Cryptomonnaies';
-} elseif ($currentURL === 'blockchain') {
-    $pageTitle = 'Blockchain';
+if ($currentURL === 'quizzblockchain') {
+    $pageTitle = 'Quizz Blockchain';
+    $crypto = 'cryptoblockchain.png';
+} elseif ($currentURL === 'quizzdanger') {
+    $crypto = 'cryptodanger.png';
+    $pageTitle = 'Quizz Danger';
+} elseif ($currentURL === 'quizzcrypto') {
+    $crypto ="cryptobitcoin.png";
+    $pageTitle = 'Quizz Crypto';
+} elseif ($currentURL === 'quizznft') {
+    $crypto = "cryptoNFT.png";
+    $pageTitle = 'Quizz NFT';
 }
 
 ?>
@@ -23,7 +24,12 @@ if ($currentURL === 'explication') {
     <a href='/pages/home' class='nav-link d-flex align-items-center'><?= $this->Html->image('acceuil.png', ['class' => 'img-fluid','alt' => 'acceuil']); ?></a>
     <a  href='/pages/tempreel' class='nav-link d-flex align-items-center'><?= $this->Html->image('temp reel.png', ['class' => 'img-fluid h-100','alt' => 'icone temp réel']); ?></a>
 
+
+    <?= $this->Html->image($crypto, ['class' => 'img-fluid h-100','alt' => 'icone temp réel']); ?>
     <div class="bg-warning rounded-pill col-5 mx-auto">
         <h1 class="h1 text-center"><?= $pageTitle ?></h1>
     </div>
+
+
 </nav>
+
