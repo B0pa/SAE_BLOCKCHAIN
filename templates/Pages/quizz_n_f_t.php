a783e755311d863210584f296e890cbe65ce0854
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2023-11-22 10:52:53 +0100

    animation home page

--- templates/Pages/quizz_n_f_t.php
+++ templates/Pages/quizz_n_f_t.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Quiz sur la NFT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <style>
     input:checked~label {
@@ -16,10 +17,6 @@
 </nav>
 <main class="pt-5 mt-5">
     <div id="quiz-container d-flex flex-column">
-        <div class="pt-3">
-
-            <p id="score">NFT : 0</p>
-        </div>
         <div id="quizzes">
             <!-- Cet élément sera utilisé pour afficher les quiz (ajoutés automatiquement) -->
         </div>
