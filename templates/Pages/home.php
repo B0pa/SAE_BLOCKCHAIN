96f79f02bf0345115053040dbe6fd6abb80cc749
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-11-22 16:03:10 +0100

    lucas push video

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -69,18 +69,21 @@
         </div>
     </main>
 
-    <aside class="slideFromTop home-aside flex-shrink-0 text-white bg-dark rounded col-0 col-md-4 p-3 mt-4">
+    <aside class="home-aside flex-shrink-0 text-white bg-dark rounded col-0 col-md-4 p-3 mt-4">
         <div class="text-center" >
             <h2 class="h2">Les actualités</h2>
         </div>
         <div class="bg-secondary rounded-2 p-2" >
-            <h3 class="h3 text-center">blabla</h3>
-            <p class="text-center" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur provident veniam nihil nemo asperiores? Possimus rem quod officia similique eaque laudantium aperiam, doloremque molestias magni earum vel quis dolorem natus!</p>
+            <h3 class="h3 text-center">Quelques liens de médias mis a jour régulièrement que nous trouvons intéressants</h3>
+            <p class="text-center" >La dernière vidéo de Hasheur:</p>
+            <iframe
+                src="https://www.youtube-nocookie.com/embed?listType=playlist&list=UULFhlTcWDE8gd4tsl_L727NrQ"
+                width="100%"
+                height="240"
+                allowfullscreen>
+            </iframe>
         </div>
-
-
-            <a href="/pages/actuality" class="text-white text-decoration-none btn btn-secondary align-self-end" >Actualité</a>
-
+        <a href="/pages/actualite" class="text-white text-decoration-none btn btn-secondary align-self-end" >Actualité</a>
     </aside>
 </div>
 
