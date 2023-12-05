ff6df46713d2d52c99a8eb0f4e7077998c4c391b
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-12-05 12:00:34 +0100

    nav_admin.php reparation titre

--- templates/cell/Article/display.php
+++ templates/cell/Article/display.php
@@ -21,7 +21,7 @@
         <div class='d-flex flex-column bg-dark text-white col-10 mx-auto my-4 p-2 rounded-3 slideFromTop' >
 
             <h2 class="h2 text-center mt-1 p-2" ><?= $actu->title ?></h2>
-            <?= $this->Html->image("upload/" . $actus->image, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => ''])?>
+            <?= $this->Html->image("../upload/" . $actu->image, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'image','style' => ''])?>
             <a href=<?= $actu->link?>><?= $actu->title ?></a>
             <p class="d-flex p-2 col-10 mx-auto" style="text-align: justify;" ><?= $actu->content?></p>
         </div>
