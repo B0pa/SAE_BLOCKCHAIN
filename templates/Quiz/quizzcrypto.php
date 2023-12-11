3a3056e0e7224e6c6478849502cba5e71a14bb2f
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-12-11 15:27:49 +0100

    Quiz thomas D

--- templates/Quiz/quizzcrypto.php
+++ templates/Quiz/quizzcrypto.php
@@ -1,43 +1,43 @@
 <?php
-
 /** @var \App\Model\Entity\Quiz[] $quizes */
-
 foreach ($quizes as $quiz) :
+    echo $this->Form->create($quiz);
     ?>
-    <p><?= $quiz->level ?></p>
-    <h2><?= $quiz->question ?></h2>
-    <?php if ($quiz->questionform == "text") : ?>
 
-    <p><?= $quiz->answer1 ?></p>
-    <p><?= $quiz->answer2 ?></p>
-    <p><?= $quiz->answer3 ?></p>
-
-<?php endif; ?>
-    <?php if ($quiz->questionform == "image") :?>
-    <?= $this->Html->image("upload/" . $quiz->answer1)?>
-    <?= $this->Html->image("upload/" . $quiz->answer2)?>
-    <?= $this->Html->image("upload/" . $quiz->answer3)?>
-<?php endif; ?>
-
+    <div class="d-flex flex-column bg-dark text-white col-10 mx-auto my-4 p-2 rounded-3 slideFromTop">
+        <p><?= $quiz->level ?></p>
+        <h2><?= $quiz->question ?></h2>
+        <?php
+        if ($quiz->questionform == "text"): // reponse au format texte
+            echo $this->Form->control('reponse', [
+                'type' => 'radio',
+                'options' => [
+                    1 => $quiz->answer1,
+                    2 => $quiz->answer2,
+                    3 => $quiz->answer3
+                ],
+                'class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => ''
+            ]);
+        endif;
+        ?>
+        <?php if ($quiz->questionform == "image") :?>
+            <label>
+                <?= $this->Form->radio('reponse', ['value' => 1]) ?>
+                <?= $this->Html->image("upload/" . $quiz->answer1, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
+            </label>
+            <label>
+                <?= $this->Form->radio('reponse', ['value' => 2]) ?>
+                <?= $this->Html->image("upload/" . $quiz->answer2, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
+            </label>
+            <label>
+                <?= $this->Form->radio('reponse', ['value' => 3]) ?>
+                <?= $this->Html->image("upload/" . $quiz->answer3, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
+            </label>
+        <?php endif; ?>
+        <?= $this->Form->submit(__('valider'), ['class' => 'btn btn-secondary']); ?>
+        <?= $this->Form->end() ?>
+        <?= $this->Flash->render() ?>
+    </div>
 <?php
 endforeach;
 ?>
-
-<body>
-<nav>
-    <?= $this->element('nav3')?>
-</nav>
-<main>
-    <div id="quiz-container">
-        <div class="PTop">
-<!--            <p id="score">BTC : 0</p>-->
-        </div>
-        <div id="quizzes" class="grid-container">
-            <!-- Cet élément sera utilisé pour afficher les quiz (ajoutés automatiquement) -->
-        </div>
-        <button id="submit-button" onclick="checkAnswers()">Soumettre</button>
-    </div>
-    <?= $this->Html->script('/js/quizCrypto.js') ?>
-</main>
-</body>
-</html>
