23e802b5e04c8addffbf00f55e106f262f4be684
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-12-11 17:40:53 +0100

    push nav thomas

--- templates/element/nav2.php
+++ templates/element/nav2.php
@@ -12,16 +12,19 @@ if ($currentURL === 'tempreel') {
 ?>
 
 
-<nav class="navbar navbar-dark bg-dark d-flex align-items-center fixed-top" style="height: 100px;">
-    <?= $this->Html->link(
-        $this->Html->image('acceuil.png', ['class' => 'img-fluid image-nav','alt' => 'accueil']),
-        ['controller'=> 'Pages', 'action' => 'home'],
-        [
-            'class' => 'nav-link d-flex align-items-center',
-            'escapeTitle' => false
-        ]
-    ) ?>
-    <div class="d-flex bg-warning rounded-pill col-* px-5 py-1 mx-auto align-items-center justify-content-center h-auto" style="transform: translateX(-40%);">
-        <h1 class="h1 text-center"><?= $pageTitle ?></h1>
+<nav class="container-fluid pt-3 bg-dark bg-dark d-flex fixed-top justify-content-between" style="height: 105px;">
+    <div class="container-fluid h-100 d-flex align-items-center nav-rep" >
+        <?= $this->Html->link(
+            $this->Html->image('acceuil.png', ['class' => 'img-fluid image-nav top-0 img-nav','alt' => 'accueil']),
+            ['controller'=> 'Pages', 'action' => 'home'],
+            [
+                'class' => 'nav-link d-flex align-items-center',
+                'escapeTitle' => false
+            ]
+        ) ?>
+        <div class="d-flex  bg-warning rounded-pill px-5 py-1 align-items-center justify-content-center h-auto title-nav centrer">
+            <h1 class="h1 text-center"><?= $pageTitle ?></h1>
+        </div>
     </div>
+
 </nav>
--- templates/element/nav_admin.php
+++ templates/element/nav_admin.php
@@ -20,27 +20,28 @@ if ($currentURL === 'update') {
 ?>
 
 
-<nav class="navbar navbar-dark bg-dark d-flex align-items-center fixed-top " style="height: 100px;">
+<nav class="container-fluid pt-3 bg-dark bg-dark d-flex fixed-top justify-content-between" style="height: 105px;">
+    <div class="container-fluid h-100 d-flex align-items-center nav-rep" >
+        <?= $this->Html->link(
+            $this->Html->image('home-update.png', ['class' => 'img-fluid image-nav top-0 img-nav bg-warning rounded-circle ','alt' => 'icon home admin']),
+            ['controller'=> 'Users', 'action' => 'update'],
+            [
+                'class' => 'nav-link d-flex align-items-center',
+                'escapeTitle' => false
+            ]
+        ) ?>
 
-    <?= $this->Html->link(
-        $this->Html->image('home-update.png', ['class' => 'img-fluid h-100 image-nav bg-warning rounded-circle ','alt' => 'icon home admin']),
-        ['controller'=> 'Users', 'action' => 'update'],
-        [
-            'class' => 'nav-link d-flex align-items-center',
-            'escapeTitle' => false
-        ]
-    ) ?>
+        <div class="bg-warning rounded-pill col-* px-5 py-1 mx-auto">
+            <h1 class="h1 text-center"><?= $pageTitle ?></h1>
+        </div>
 
-    <div class="bg-warning rounded-pill col-* px-5 py-1 mx-auto">
-        <h1 class="h1 text-center"><?= $pageTitle ?></h1>
+        <?= $this->Html->link(
+            $this->Html->image('logout.png', ['class' => 'img-fluid image-nav top-0 img-nav bg-warning rounded-circle','alt' => 'icone logout']),
+            ['controller'=> 'Users', 'action' => 'logout'],
+            [
+                'class' => 'nav-link d-flex align-items-center',
+                'escapeTitle' => false
+            ]
+        ) ?>
     </div>
-
-    <?= $this->Html->link(
-        $this->Html->image('logout.png', ['class' => 'img-fluid h-100 image-nav bg-warning rounded-circle p-2 ','alt' => 'icone logout']),
-        ['controller'=> 'Users', 'action' => 'logout'],
-        [
-            'class' => 'nav-link d-flex align-items-center',
-            'escapeTitle' => false
-        ]
-    ) ?>
 </nav>
