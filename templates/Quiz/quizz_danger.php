88853f3f6347d48dcd6c0caefdb019ed9a7036cf
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2023-12-22 11:00:10 +0100

    commit thomas +  thomas quizz graph

--- templates/Quiz/quizz_danger.php
+++ templates/Quiz/quizz_danger.php
@@ -3,16 +3,22 @@
 <?php
 /** @var \App\Model\Entity\Quiz[] $quizes */
 foreach ($quizes as $index => $quiz) :
-    echo $this->Form->create($quiz);
+echo $this->Form->create($quiz);
 
-    $csv_link = $quiz->csv_link;
+$csv_link = $quiz->csv_link;
 
-    ?>
+
+?>
+<body class="bg-secondary" >
+<header>
     <nav>
         <?= $this->element('nav3')?>
     </nav>
+</header>
+<main class="pt-5 mt-5" >
 
     <div class="d-flex flex-column bg-dark text-white col-10 mx-auto my-4 p-2 rounded-3 slideFromTop">
+        <p><?= $quiz->level ?></p>
         <?php if ($quiz->questionform == "graphic") :?>
             <p><?= $quiz->question ?></p>
 
@@ -71,52 +77,45 @@ foreach ($quizes as $index => $quiz) :
         <?php endif; ?>
         <p><?= $quiz->level ?></p>
         <?php
-        if ($quiz->questionform == "text"): // reponse au format texte
-            echo $this->Form->control('reponse', [
-                'type' => 'radio',
-                'options' => [
-                    1 => $quiz->answer1,
-                    2 => $quiz->answer2,
-                    3 => $quiz->answer3
-                ],
-                'class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => ''
-            ]);
-
-            ?>
-        <?php endif; ?>
-        <?php
-        if ($quiz->questionform == "graphic"): // reponse au format texte
-            echo $this->Form->control('reponse', [
-                'type' => 'radio',
-                'options' => [
-                    1 => $quiz->answer1,
-                    2 => $quiz->answer2,
-                    3 => $quiz->answer3
-                ],
-                'class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => ''
-            ]);
-
-            ?>
-        <?php endif; ?>
+        if ($quiz->questionform == "text"): ?>
+            <label class="text-white" >
+                <?= $this->Form->control('reponse', [
+                    'type' => 'radio',
+                    'options' => [1 => $quiz->answer1],
+                    'label' => false]) ?>
+            </label>
+            <label class="text-white" >
+                <?= $this->Form->control('reponse', [
+                    'type' => 'radio',
+                    'options' => [1 => $quiz->answer2],
+                    'label' => false]) ?>
+            </label>
+            <label class="text-white" >
+                <?= $this->Form->control('reponse', [
+                    'type' => 'radio',
+                    'options' => [1 => $quiz->answer3],
+                    'label' => false]) ?>
+            </label>
+        <?php endif;
+        ?>
         <?php if ($quiz->questionform == "image") :?>
-            <label>
+            <label class="d-flex justify-content-around" >
                 <?= $this->Form->radio('reponse', ['value' => 1]) ?>
-                <?= $this->Html->image("upload/" . $quiz->answer1, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
-            </label>
-            <label>
+                <?= $this->Html->image("upload/" . $quiz->answer1, ['class' => 'd-flex img-fluid w-50 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
+
                 <?= $this->Form->radio('reponse', ['value' => 2]) ?>
-                <?= $this->Html->image("upload/" . $quiz->answer2, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
-            </label>
-            <label>
+                <?= $this->Html->image("upload/" . $quiz->answer2, ['class' => 'd-flex img-fluid w-50 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
+
                 <?= $this->Form->radio('reponse', ['value' => 3]) ?>
-                <?= $this->Html->image("upload/" . $quiz->answer3, ['class' => 'd-flex img-fluid w-75 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
+                <?= $this->Html->image("upload/" . $quiz->answer3, ['class' => 'd-flex img-fluid w-50 mx-auto rounded-3 mt-2 mb-3','alt' => 'accueil','style' => '']) ?>
             </label>
         <?php endif; ?>
         <?= $this->Form->submit(__('valider'), ['class' => 'btn btn-secondary']); ?>
         <?= $this->Form->end() ?>
         <?= $this->Flash->render() ?>
     </div>
-
-<?php
-endforeach;
-?>
+    <?php
+    endforeach;
+    ?>
+</main>
+</body>
