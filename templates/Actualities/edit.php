f019e9376823a7e625adfcefbfd77c1f6f6a213d
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2023-12-22 11:36:48 +0100

    CSS : Actualities, add,edit,view

--- templates/Actualities/edit.php
+++ templates/Actualities/edit.php
@@ -4,32 +4,49 @@
  * @var \App\Model\Entity\Actuality $actuality
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $actuality->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $actuality->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Actualities'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+<body class="bg-secondary pt-5 mt-5" >
+<?= $this->element('nav_admin')?>
+    <main class="mt-5 pt-3" >
+        <div class="row col-12 p-3">
+            <aside class="col">
+                <div class="side-nav">
+                    <h4 class="heading"><?= __('Actions') ?></h4>
+                    <?= $this->Form->postLink(
+                        __('Delete'),
+                        ['action' => 'delete', $actuality->id],
+                        ['confirm' => __('Are you sure you want to delete # {0}?', $actuality->id), 'class' => 'side-nav-item']
+                    ) ?>
+                    <?= $this->Html->link(__('List Actualities'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+                </div>
+            </aside>
+            <div class="col-9 p-3 bg-dark rounded text-white">
+                <div class="actualities content">
+                    <?= $this->Form->create($actuality) ?>
+                    <fieldset>
+                        <legend><?= __('Edit Actuality') ?></legend>
+                        <?php
+                            echo $this->Form->control('title', [
+                                'class' => 'form-control bg-secondary'
+                            ]);
+                            echo $this->Form->control('text', [
+                                'class' => 'form-control bg-secondary'
+                            ]);
+                            echo $this->Form->control('link', [
+                                'class' => 'form-control bg-secondary'
+                            ]);
+                            echo $this->Form->control('img', [
+                                'type' => 'file',
+                                'label' => 'Votre jolie image',
+                                'class' => 'form-control bg-secondary',
+                                'after' => $this->Form->button('Modifier l\'image', ['type' => 'button', 'class' => 'btn btn-secondary mt-3', 'id' => 'edit-upload-btn', 'data-target' => '#upload-input']),
+                            ]);
+                        ?>
+                    </fieldset>
+                    <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-secondary mt-3']) ?>
+                    <?= $this->Form->end() ?>
+                </div>
+            </div>
         </div>
-    </aside>
-    <div class="column column-80">
-        <div class="actualities form content">
-            <?= $this->Form->create($actuality) ?>
-            <fieldset>
-                <legend><?= __('Edit Actuality') ?></legend>
-                <?php
-                    echo $this->Form->control('title');
-                    echo $this->Form->control('text');
-                    echo $this->Form->control('link');
-                    echo $this->Form->control('img');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
-</div>
+    </main>
+</body>
+
--- templates/Actualities/view.php
+++ templates/Actualities/view.php
@@ -4,43 +4,49 @@
  * @var \App\Model\Entity\Actuality $actuality
  */
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Actuality'), ['action' => 'edit', $actuality->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Actuality'), ['action' => 'delete', $actuality->id], ['confirm' => __('Are you sure you want to delete # {0}?', $actuality->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Actualities'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Actuality'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="actualities view content">
-            <h3><?= h($actuality->title) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Title') ?></th>
-                    <td><?= h($actuality->title) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Link') ?></th>
-                    <td><?= h($actuality->link) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Img') ?></th>
-                    <td><?= h($actuality->img) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($actuality->id) ?></td>
-                </tr>
-            </table>
-            <div class="text">
-                <strong><?= __('Text') ?></strong>
-                <blockquote>
-                    <?= $this->Text->autoParagraph(h($actuality->text)); ?>
-                </blockquote>
+<body class="mt-5 pt-5 bg-secondary" >
+<?= $this->element('nav_admin')?>
+<main class="pt-3 p-3" >
+    <div class="row">
+        <aside class="column">
+            <div class="side-nav">
+                <h4 class="heading"><?= __('Actions') ?></h4>
+                <?= $this->Html->link(__('Edit Actuality'), ['action' => 'edit', $actuality->id], ['class' => 'side-nav-item']) ?>
+                <?= $this->Form->postLink(__('Delete Actuality'), ['action' => 'delete', $actuality->id], ['confirm' => __('Are you sure you want to delete # {0}?', $actuality->id), 'class' => 'side-nav-item']) ?>
+                <?= $this->Html->link(__('List Actualities'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+                <?= $this->Html->link(__('New Actuality'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
+            </div>
+        </aside>
+        <div class="column column-80">
+            <div class="actualities view content">
+                <h3><?= h($actuality->title) ?></h3>
+                <table>
+                    <tr>
+                        <th><?= __('Title') ?></th>
+                        <td><?= h($actuality->title) ?></td>
+                    </tr>
+                    <tr>
+                        <th><?= __('Link') ?></th>
+                        <td><?= h($actuality->link) ?></td>
+                    </tr>
+                    <tr>
+                        <th><?= __('Img') ?></th>
+                        <td><?= h($actuality->img) ?></td>
+                    </tr>
+                    <tr>
+                        <th><?= __('Id') ?></th>
+                        <td><?= $this->Number->format($actuality->id) ?></td>
+                    </tr>
+                </table>
+                <div class="text">
+                    <strong><?= __('Text') ?></strong>
+                    <blockquote>
+                        <?= $this->Text->autoParagraph(h($actuality->text)); ?>
+                    </blockquote>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</main>
+</body>
+
