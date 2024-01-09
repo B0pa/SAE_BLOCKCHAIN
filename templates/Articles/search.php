a748cbd1a6a76f4d9774d8b099035a8637b4888b
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2024-01-09 16:40:04 +0100

    recherche finit

--- templates/Articles/search.php
+++ templates/Articles/search.php
@@ -1,6 +1,7 @@
 <body class="bg-secondary" >
 <?= $this->element('nav')?>
 <main class="pt-5 mt-5" >
+    <?php  /** @var \App\Model\Entity\Article[] $category */ ?>
 
     <form action="<?= $this->Url->build(['controller' => 'Articles', 'action' => 'search', $category]) ?>" method="get">
         <input type="text" name="query" placeholder="Rechercher...">
@@ -12,9 +13,10 @@
     foreach ($articles as $article) :
         ?>
         <div class='d-flex flex-column bg-dark text-white col-10 mx-auto my-4 p-2 rounded-3' >
-            <h2><?= $article->title ?></h2>
-            <p style="text-align: justify;" ><?= nl2br($article->content)?></p>
-            <?= $this->Html->image("upload/" . $article->image, ['alt' => 'article image','style' => ''])?>
+            <p class="d-flex p-2 col-10 mx-auto" ><?= $article->level?></p>
+            <h2 class=<?= $article->css_title ?> ><?= $article->title ?></h2>
+            <p class=<?= $article->css_content ?> style="text-align: justify;" ><?= nl2br($article->content)?></p>
+            <?= $this->Html->image("upload/" . $article->image, ['class' => $article->css_img ,'alt' => 'accueil','style' => ''])?>
         </div>
     <?php
     endforeach;
