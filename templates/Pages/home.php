d8c3050d202d75a5240606020eb3869fe277d3ec
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-01-10 13:57:48 +0100

    HOME BOUTONS ENLEVER VERS INFOS

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -40,84 +40,45 @@
 </header>
 <div class="d-flex flex-column flex-md-row mx-1 mt-5 pt-5 mx-md-5 " style="height:115vh" >
     <main class="col col-8">
-        <div class=" d-flex slideFromTop p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 justify-content-between  ">
-
-            <?= $this->Html->image('NFT.png', ['alt' => 'icone NFT', 'style' => 'height:100px' , 'class' => 'img-thumbnail img-fluid']); ?>
-
-
-            <div class="px-3" >
-                <h2 class="h2 text-center">Les NFT</h2>
-                <p>Jetons numériques uniques représentant un objet virtuel. Spéculatifs et sujets aux arnaques.</p>
+        <a class="text-decoration-none " href="<?= $this->Url->build(['controller'=> 'Articles', 'action' => 'nft']) ?>">
+            <div class=" d-flex slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 justify-content-between  ">
+                <?= $this->Html->image('NFT.png', ['alt' => 'icone NFT', 'style' => 'height:100px' , 'class' => 'img-thumbnail img-fluid']); ?>
+                <div class="px-3" >
+                    <h2 class="h2 text-center">Les NFT</h2>
+                    <p>Jetons numériques uniques représentant un objet virtuel. Spéculatifs et sujets aux arnaques.</p>
+                </div>
             </div>
+        </a>
 
-             <?= $this->Html->link(
-                "NFT",
-                ['controller'=> 'Articles', 'action' => 'nft'],
-                [
-                    'class' => 'text-white text-decoration-none btn btn-secondary align-self-end',
-                    'escapeTitle' => false
-                ]
-            ) ?>
-        </div>
-
-        <div class="slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex justify-content-between">
-
-            <?= $this->Html->image('crypto.png', ['alt' => 'icone cryptomonnais', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-
-            <div class="px-3" >
-                <h2 class="h2 text-center">Les Crypto</h2>
-                <p>monnaies numériques basées sur la blockchain, comme le Bitcoin. Volatiles et risquées.</p>
+        <a class="text-decoration-none " href="<?= $this->Url->build(['controller'=> 'Articles', 'action' => 'crypto']) ?>">
+            <div class="slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex justify-content-between">
+                <?= $this->Html->image('crypto.png', ['alt' => 'icone cryptomonnais', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
+                <div class="px-3" >
+                    <h2 class="h2 text-center">Les Crypto</h2>
+                    <p>monnaies numériques basées sur la blockchain, comme le Bitcoin. Volatiles et risquées.</p>
+                </div>
             </div>
+        </a>
 
-            <?= $this->Html->link(
-                "crypto",
-                ['controller'=> 'Articles', 'action' => 'crypto'],
-                [
-                    'class' => 'text-white text-decoration-none btn btn-secondary align-self-end',
-                    'escapeTitle' => false
-                ]
-            ) ?>
-        </div>
-
-        <div class="slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex justify-content-between" >
-
-            <?= $this->Html->image('danger.png', ['alt' => 'icone danger', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-
-            <div class="px-3" >
-                <h2 class="h2 text-center" >Les dangers</h2>
-                <p>volatilité, hacking, arnaques, bulles spéculatives, utilisation à des fins illicites, impact environnemental (mining).</p>
+        <a class="text-decoration-none " href="<?= $this->Url->build(['controller'=> 'Articles', 'action' => 'danger']) ?>">
+            <div class="slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex justify-content-between">
+                <?= $this->Html->image('danger.png', ['alt' => 'icone danger', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
+                <div class="px-3" >
+                    <h2 class="h2 text-center">Les dangers</h2>
+                    <p>volatilité, hacking, arnaques, bulles spéculatives, utilisation à des fins illicites, impact environnemental (mining).</p>
+                </div>
             </div>
-            
-            <?= $this->Html->link(
-                "Danger",
-                ['controller'=> 'Articles', 'action' => 'danger'],
-                [
-                    'class' => 'text-white text-decoration-none btn btn-secondary align-self-end',
-                    'escapeTitle' => false
-                ]
-            ) ?>
-        </div>
+        </a>
 
-        <div class="slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex justify-content-between" >
-
-
-            <?= $this->Html->image('blockchain.jpg', ['alt' => 'blockchain', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
-            
-            <div class="px-3" >
-                <h2 class="h2 text-center">Les blockchain</h2>
-                <p>technologie de stockage et de transmission d'informations, transparente, sécurisée et décentralisée.</p>
+        <a class="text-decoration-none " href="<?= $this->Url->build(['controller'=> 'Articles', 'action' => 'blockchain']) ?>">
+            <div class="slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 d-flex justify-content-between">
+                <?= $this->Html->image('blockchain.jpg', ['alt' => 'blockchain', 'style' => 'height:100px', 'class' => 'img-thumbnail']); ?>
+                <div class="px-3" >
+                    <h2 class="h2 text-center">Les blockchain</h2>
+                    <p>technologie de stockage et de transmission d'informations, transparente, sécurisée et décentralisée.</p>
+                </div>
             </div>
-            
-           <?= $this->Html->link(
-                "Blockchain",
-                ['controller'=> 'Articles', 'action' => 'blockchain'],
-                [
-                    'class' => 'text-white text-decoration-none btn btn-secondary align-self-end',
-                    'escapeTitle' => false
-                ]
-            ) ?>
-
-        </div>
+        </a>
 
         <div class="slideFromTop grow p-3 bg-dark text-white rounded mt-4 p-3 col-0 col-md-11 justify-content-between">
             <h2 class="h2 text-center" >Qui sommes-nous ?</h2>
