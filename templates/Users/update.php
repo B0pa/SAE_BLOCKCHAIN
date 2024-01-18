754e45227876fe59289fdc1b520041cbec765540
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-01-18 09:36:50 +0100

    CSS UPDATE non scroll

--- templates/Users/update.php
+++ templates/Users/update.php
@@ -1,95 +1,89 @@
 <body class="pt-5 bg-secondary h-100 " >
 <?= $this->element('nav_admin')?>
-<main class="mt-5 pt-5 h-100" >
+<main class="mt-5 pt-3 h-100" >
     <div  id="update-tab-menu" class=" container h-100" >
         <a href="/users/register" class="tab text-decoration-none text-dark text-center grow" >
-            <div class="my-auto">
+            <div class="my-auto h-75">
                 <h2>Ajouter un administrateur</h2>
-                <?= $this->Html->image('administrator.png', ['class' => 'img-fluid w-50 mt-3 mx-auto','alt' => 'NFT image']); ?>
+                <?= $this->Html->image('administrator.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
             </div>
-
         </a>
 
         <div class="tab text-center" >
             <h2 class="mt-3" >Modifier des Actualité</h2>
-            <div class="mt-3 update-tab-contenu">
-                <?= $this->Html->image('actualite.png', ['class' => 'img-fluid w-50 mt-3 mx-auto','alt' => 'NFT image']); ?>
-                <div class="d-flex justify-content-around  " >
-                    <?= $this->Html->link(
-                        "Ajouter Actualité",
-                        ['controller'=> 'Actualities', 'action' => 'add'],
-                        [
-                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-4 m-3',
-                            'escapeTitle' => false
-                        ]
-                    ) ?>
-
-                    <?= $this->Html->link(
-                        "Modifier Actualité",
-                        ['controller'=> 'Actualities', 'action' => 'index'],
-                        [
-                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-4 m-3',
-                            'escapeTitle' => false
-                        ]
-                    ) ?>
+            <div class="d-flex justify-content-around h-75" >
+                <?= $this->Html->link(
+                    "Ajouter Actualité",
+                    ['controller'=> 'Actualities', 'action' => 'add'],
+                    [
+                        'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                        'escapeTitle' => false
+                    ]
+                ) ?>
+                <div style="height: 75%;">
+                    <?= $this->Html->image('actualite.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
                 </div>
+                <?= $this->Html->link(
+                    "Modifier Actualité",
+                    ['controller'=> 'Actualities', 'action' => 'index'],
+                    [
+                        'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                        'escapeTitle' => false
+                    ]
+                ) ?>
             </div>
         </div>
 
+
         <div class="tab text-center" >
             <h2 class="mt-3" >Modifier des Quiz</h2>
-            <div class="mt-3 update-tab-contenu">
-                <?= $this->Html->image('quiz.png', ['class' => 'img-fluid w-50 mt-3 mx-auto','alt' => 'NFT image']); ?>
-                <div class="d-flex justify-content-around  " >
-                    <?= $this->Html->link(
-                        "Ajouter Quiz",
-                        ['controller'=> 'Quiz', 'action' => 'add'],
-                        [
-                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-4 m-3',
-                            'escapeTitle' => false
-                        ]
-                    ) ?>
-
-                    <?= $this->Html->link(
-                        "Modifier Quiz",
-                        ['controller'=> 'Quiz', 'action' => 'index'],
-                        [
-                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-4 m-3',
-                            'escapeTitle' => false
-                        ]
-                    ) ?>
+            <div class="d-flex justify-content-around  h-75" >
+                <?= $this->Html->link(
+                    "Ajouter Quiz",
+                    ['controller'=> 'Quiz', 'action' => 'add'],
+                    [
+                        'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                        'escapeTitle' => false
+                    ]
+                ) ?>
+                <div style="height: 75%;">
+                    <?= $this->Html->image('quiz.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
                 </div>
+               
+                <?= $this->Html->link(
+                    "Modifier Quiz",
+                    ['controller'=> 'Quiz', 'action' => 'index'],
+                    [
+                        'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                        'escapeTitle' => false
+                    ]
+                ) ?>
             </div>
         </div>
 
         <div class="tab text-center">
             <h2 class="mt-3" >Modifier des informations</h2>
-            <div class="mt-3 update-tab-contenu">
-                <?= $this->Html->image('explication.png', ['class' => 'img-fluid w-50 mt-3 mx-auto','alt' => 'NFT image']); ?>
-                <div class="d-flex justify-content-around  " >
+                <div class="d-flex justify-content-around  h-75" >
                     <?= $this->Html->link(
                         "Ajouter Articles",
                         ['controller'=> 'Articles', 'action' => 'add'],
                         [
-                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-4 m-3',
+                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
                             'escapeTitle' => false
                         ]
                     ) ?>
-
+                    <div style="height: 75%;">
+                        <?= $this->Html->image('explication.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
+                    </div>
                     <?= $this->Html->link(
                         "Modifier Articles",
                         ['controller'=> 'Articles', 'action' => 'index'],
                         [
-                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-4 m-3',
+                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
                             'escapeTitle' => false
                         ]
                     ) ?>
                 </div>
-
-
-            </div>
-
-
         </div>
     </div>
 
