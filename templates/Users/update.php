ba7b11a10125da6c29db9f508f76e9800b16deaf
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-01-19 10:06:07 +0100

    NO SCROLL UPDATE

--- templates/Users/update.php
+++ templates/Users/update.php
@@ -63,30 +63,29 @@
 
         <div class="tab text-center">
             <h2 class="mt-3" >Modifier des informations</h2>
-            <div class="d-flex justify-content-around  h-75" >
-                <?= $this->Html->link(
-                    "Ajouter Articles",
-                    ['controller'=> 'Articles', 'action' => 'add'],
-                    [
-                        'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
-                        'escapeTitle' => false
-                    ]
-                ) ?>
-                <div style="height: 75%;">
-                    <?= $this->Html->image('explication.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
+                <div class="d-flex justify-content-around  h-75" >
+                    <?= $this->Html->link(
+                        "Ajouter Articles",
+                        ['controller'=> 'Articles', 'action' => 'add'],
+                        [
+                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                            'escapeTitle' => false
+                        ]
+                    ) ?>
+                    <div style="height: 75%;">
+                        <?= $this->Html->image('explication.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
+                    </div>
+                    <?= $this->Html->link(
+                        "Modifier Articles",
+                        ['controller'=> 'Articles', 'action' => 'index'],
+                        [
+                            'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                            'escapeTitle' => false
+                        ]
+                    ) ?>
                 </div>
-                <?= $this->Html->link(
-                    "Modifier Articles",
-                    ['controller'=> 'Articles', 'action' => 'index'],
-                    [
-                        'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
-                        'escapeTitle' => false
-                    ]
-                ) ?>
-            </div>
         </div>
     </div>
-
 
 </main>
 </body>
