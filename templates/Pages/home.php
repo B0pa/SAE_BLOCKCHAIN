fb66fa63a6ed2292274cbee8335627e473645f0e
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-01-19 10:00:59 +0100

    CSS popup

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -47,19 +47,19 @@ use Cake\Log\Log; ?>
     <main class="col col-12 col-md-8">
 
 
-        <div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
+    <div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
+            <div class="modal-dialog text-white">
+                <div class="modal-content bg-secondary border-3 border-dark">
+                    <div class="modal-header border-dark">
                         <h5 class="modal-title" id="cookieModalLabel">Politique des cookies</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn-close " data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        Ce site utilise des cookies pour améliorer votre expérience. En continuant à utiliser ce site, vous acceptez notre utilisation des cookies.
+                        Ce site utilise des cookies pour améliorer votre expérience. Ils servent à sauvegarder votre score. En continuant à utiliser ce site, vous acceptez notre utilisation des cookies.
                     </div>
-                    <div class="modal-footer">
+                    <div class="modal-footer border-dark">
                         <?php echo $this->Form->create(null, ['url' => ['action' => 'cookieAccept']]) ;?>
-                        <?= $this->Form->button(__('Accept'), ['class' => 'btn btn-secondary bg-dark text-white rounded-3 slideFromTop ']) ?>
+                        <?= $this->Form->button(__('Accept'), ['class' => 'btn btn-warning text-white rounded-3 slideFromTop ']) ?>
                         <?= $this->Form->end() ?>
                         <?php echo $this->Form->create(null, ['url' => ['action' => 'cookieRefuse']]) ;?>
                         <?= $this->Form->button(__('Refuse'), ['class' => 'btn btn-secondary bg-dark text-white rounded-3 slideFromTop ']) ?>
