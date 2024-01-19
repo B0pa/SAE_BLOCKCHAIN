438a956bdf2098eae16aa653a95a886b82558d13
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2024-01-19 22:50:51 +0100

    save temp reel

--- templates/Pages/tempreel.php
+++ templates/Pages/tempreel.php
@@ -1,58 +1,65 @@
 
 <head>
-
     <title>Temp réel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@1.1.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.3.2/dist/chart.min.js"></script>
+
 </head>
+
 <body>
 <?= $this->element('nav2')?>
-<main>
-    <div>
-        <h1>bitcoin</h1>
-        <canvas id="chartContainer"></canvas>
-    </div>
-</main>
 
+<main>
+    <canvas id="chart"></canvas>
+
+</main>
 <script>
-    var ctx = document.getElementById('chartContainer').getContext('2d');
+
+    // Récupérer le canvas
+    var ctx = document.getElementById('chart').getContext('2d');
+
+    // Graphique
     var chart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: [],
             datasets: [{
-                label: 'Bitcoin Price',
-                data: [],
-                borderColor: 'rgba(75, 192, 192, 1)',
-                fill: false
+                data: []
             }]
-        },
-        options: {
-            scales: {
-                x: {
-                    type: 'time',
-                    time: {
-                        unit: 'minute'
-                    }
-                }
-            }
         }
     });
 
-    function fetchBitcoinPrice() {
-        fetch('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
-            .then(response => response.json())
+    // Fetch des données
+    function fetchData() {
+        fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(res => res.json())
             .then(data => {
-                var date = new Date();
-                chart.data.labels.push(date);
-                chart.data.datasets[0].data.push(data.bpi.USD.rate_float);
+                console.log(data);
+                // Vérifier les données
+                if(!data || !data.bpi || !data.bpi.USD) {
+                    return;
+                }
+
+                // Cours en USD
+                var price = parseFloat(data.bpi.USD.rate.replace(/,/g, ''));
+
+                // Ajouter au graph
+                var timestamp = new Date();
+                chart.data.labels.push(timestamp);
+                chart.data.datasets[0].data.push(price);
+
+                // Update
                 chart.update();
+
             });
     }
 
-    fetchBitcoinPrice();
-    setInterval(fetchBitcoinPrice, 60000); // Update every minute
+    // Premier appel
+    fetchData();
+
+    // Refresh toutes les minutes
+    setInterval(fetchData, 60000);
+
 </script>
 
 </body>
