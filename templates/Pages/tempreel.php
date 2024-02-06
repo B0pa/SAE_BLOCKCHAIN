2fd79bd6ee1c7805da1ac65d249383219cbd837e
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2024-02-06 17:05:07 +0100

    candle stick

--- templates/Pages/tempreel.php
+++ templates/Pages/tempreel.php
@@ -1,6 +1,6 @@
-
-
 <?= $this->element('nav')?>
+
+
 
 <main class="mt-5 pt-5" >
     <div class=" mt-3 d-flex align-items-center mx-auto w-75">
@@ -11,83 +11,141 @@
     <div class="d-flex mt-2 bg-dark border-1 border border-warning p-2 w-75 mx-auto rounded-3" >
         <canvas id="chart" class="m-2" style="max-height:60vh"></canvas>
     </div>
-
-
 </main>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@3.3.2/dist/chart.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/luxon@1.26.0"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.1/dist/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@1.0.0"></script>
+<?= $this->Html->script('chartjs-chart-financial') ?>
+
+
 <script>
-    var data = {
-        labels: [],
-        datasets: [{
-            data: [],
-            label: 'Bitcoin',
-            // barre de la courbe
-            borderColor: 'rgb(255, 193, 7)',
-        }]
-    };
-
-    var options = {
-
-        scales: {
-            x: {
-                //Axe X
-                display: true,
-                grid: {
-                    color: 'rgb(108, 117, 125)'
-                },
-                ticks: {
-                    color: 'rgb(255, 255, 255)'
-                },
-                time: {
-                    displayFormats: {
-                        minute: 'HH:mm:ss'
-                    }
-                }
-            },
-            //Axe Y
-            y: {
-                grid: {
-                    color: 'rgb(108, 117, 125)'
-                },
-                ticks: {
-                    color: 'rgb(255, 255, 255)'
-                }
-            }
-        },
-        // point de la courbe
-        elements: {
-            line: {
-                borderColor: 'rgb(0, 0, 0)'
-            },
-            point: {
-                backgroundColor: 'rgb(0, 0, 0)'
-            }
-        }
-    }
+    var barCount = 60;
+    var initialDateStr = '01 Apr 2017 00:00 Z';
 
     var ctx = document.getElementById('chart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: options
-    });
+    ctx.canvas.width = 1000;
+    ctx.canvas.height = 250;
 
-    async function fetchData() {
+    var barData = getRandomData(initialDateStr, barCount);
 
-        const now = new Date();
-
-        const response = await fetch('//api.coindesk.com/v1/bpi/currentprice.json');
-        const json = await response.json();
-        const price = parseFloat(json.bpi.USD.rate.replace(/,/g, ''));
-
-        data.labels.push(now.toLocaleTimeString());
-        data.datasets[0].data.push(price);
-
-        chart.update();
-
+    function lineData() {
+        return barData.map(d => {
+            return { x: d.x, y: d.c}
+        });
     }
 
-    setInterval(fetchData, 100 * 60);
+    var chart = new Chart(ctx, {
+        type: 'candlestick',
+        data: {
+            datasets: [{
+                label: 'CHRT - Chart.js Corporation',
+                data: barData
+            }]
+        }
+    });
 
-    fetchData();
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    function randomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    function randomBar(date, lastClose) {
+        var open = +randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
+        var close = +randomNumber(open * 0.95, open * 1.05).toFixed(2);
+        var high = +randomNumber(Math.max(open, close), Math.max(open, close) * 1.1).toFixed(2);
+        var low = +randomNumber(Math.min(open, close) * 0.9, Math.min(open, close)).toFixed(2);
+        return {
+            x: date.valueOf(),
+            o: open,
+            h: high,
+            l: low,
+            c: close
+        };
+    }
+
+    function getRandomData(dateStr, count) {
+        var date = luxon.DateTime.fromRFC2822(dateStr);
+        var data = [randomBar(date, 30)];
+        while (data.length < count) {
+            date = date.plus({days: 1});
+            if (date.weekday <= 5) {
+                data.push(randomBar(date, data[data.length - 1].c));
+            }
+        }
+        return data;
+    }
+
+    var update = function() {
+        var dataset = chart.config.data.datasets[0];
+
+        // candlestick vs ohlc
+        var type = document.getElementById('type').value;
+        dataset.type = type;
+
+        // linear vs log
+        var scaleType = document.getElementById('scale-type').value;
+        chart.config.options.scales.y.type = scaleType;
+
+        // color
+        var colorScheme = document.getElementById('color-scheme').value;
+        if (colorScheme === 'neon') {
+            dataset.color = {
+                up: '#01ff01',
+                down: '#fe0000',
+                unchanged: '#999',
+            };
+        } else {
+            delete dataset.color;
+        }
+
+        // border
+        var border = document.getElementById('border').value;
+        var defaultOpts = Chart.defaults.elements[type];
+        if (border === 'true') {
+            dataset.borderColor = defaultOpts.borderColor;
+        } else {
+            dataset.borderColor = {
+                up: defaultOpts.color.up,
+                down: defaultOpts.color.down,
+                unchanged: defaultOpts.color.up
+            };
+        }
+
+        // mixed charts
+        var mixed = document.getElementById('mixed').value;
+        if(mixed === 'true') {
+            chart.config.data.datasets = [
+                {
+                    label: 'CHRT - Chart.js Corporation',
+                    data: barData
+                },
+                {
+                    label: 'Close price',
+                    type: 'line',
+                    data: lineData()
+                }
+            ]
+        }
+        else {
+            chart.config.data.datasets = [
+                {
+                    label: 'CHRT - Chart.js Corporation',
+                    data: barData
+                }
+            ]
+        }
+
+        chart.update();
+    };
+
+    document.getElementById('update').addEventListener('click', update);
+
+    document.getElementById('randomizeData').addEventListener('click', function() {
+        barData = getRandomData(initialDateStr, barCount);
+        update();
+    });
 </script>
