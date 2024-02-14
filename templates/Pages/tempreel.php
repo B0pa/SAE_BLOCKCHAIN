7266ecb2afd87511745be6a63f65492e04595c1d
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2024-02-14 14:34:31 +0100

    toute les page article css

--- templates/Pages/tempreel.php
+++ templates/Pages/tempreel.php
@@ -7,6 +7,10 @@
     </div>
 
     <div class="" >
+        <select id="chartType" onchange="changeChartType()">
+            <option value="line">Line</option>
+            <option value="candlestick">Candlestick</option>
+        </select>
         <canvas id="chart" class="m-2" style="max-height:60vh"></canvas>
     </div>
 </main>
@@ -16,21 +20,57 @@
 <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@1.0.0"></script>
 <?= $this->Html->script('chartjs-chart-financial') ?>
 
+
 <script>
-    var ctx = document.getElementById('chart').getContext('2d');
+    // Récupération du context du canvas
+    // Récupération du context du canvas
+    const ctx = document.getElementById('chart').getContext('2d');
+
     ctx.canvas.width = 1000;
     ctx.canvas.height = 250;
 
-    var chart = new Chart(ctx, {
-        type: 'candlestick',
-        data: {
-            datasets: [{
-                label: 'BTC/USDT',
-                data: []
-            }]
-        }
-    });
+    // Création du graphique
+    var chart;
 
+    // Fonction pour créer le graphique
+    function createChart(chartType) {
+        // Destruction s'il existe déjà
+        if(chart) {
+            chart.destroy();
+        }
+
+        // Création
+        chart = new Chart(ctx, {
+            type: chartType,
+            data: {
+                datasets: [{
+                    label: 'BTCUSDT',
+                    data: [],
+                    borderColor: 'yellow',
+                }]
+            },
+            options: {
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'minute'
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    // Formatter pour les données en mode Line
+    function formatLine(candle) {
+        return {
+            x: candle[0],
+            y: candle[4]
+        }
+    }
+
+    // Formatter pour les données en mode Candlestick
     function formatBar(candle) {
         return {
             x: candle[0], // Pass the timestamp directly
@@ -41,25 +81,39 @@
         };
     }
 
-    function updateChart() {
-        fetch('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m&limit=60')
-            .then(response => response.json())
-            .then(data => {
-                var formattedData = data.map(formatBar);
+    // Fonction de mise à jour des données
+    async function updateData() {
+        // Récupération des données
+        const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1m';
+        const res = await fetch(url);
+        const rawData = await res.json();
 
-                // insertion dans le graphique
-                chart.data.datasets[0].data = []; // Clear the existing data
-                chart.data.datasets[0].data = formattedData; // Add the new data
-                chart.update();
-            })
-            .catch(error => {
-                console.error('An error occurred: ', error);
-            });
+        // Formatage
+        let formattedData = [];
+        if(chart.config.type === 'line') {
+            formattedData = rawData.map(formatLine);
+        } else {
+            // Take only every 10th data point for candlestick chart
+            formattedData = rawData.filter((_, index) => index % 5 === 0).map(formatBar);
+        }
+
+        // Insertion des données dans le dataset
+        chart.data.datasets[0].data = formattedData;
+
+        // Update du graphique
+        chart.update();
     }
 
-    // Appellez la fonction updateChart
-    updateChart();
+    // Boucle à interval régulier
+    setInterval(updateData, 10000);
 
-    // Appellez la fonction updateChart toutes les 10 secondes
-    setInterval(updateChart, 10000);
+    // Appel initial
+    createChart('line');
+    updateData();
+
+    // Event listener pour le select
+    document.getElementById('chartType').addEventListener('change', function() {
+        createChart(this.value);
+        updateData();
+    });
 </script>
