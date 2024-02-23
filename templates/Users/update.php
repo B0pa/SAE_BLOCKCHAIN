2c1b90784d959f50c7cd540955c247b49d48ac8a
Arnaud-Dec <arnaud.decourt@etu.univ-lyon1.fr>
2024-02-23 15:16:21 +0100

    thomas update.php

--- templates/Users/update.php
+++ templates/Users/update.php
@@ -1,14 +1,29 @@
 <main id="update-main" class="navmarge" >
-    <a href="/users/register" class="tab text-decoration-none text-dark text-center grow" >
+    <div class="tab text-decoration-none text-dark text-center" >
         <h2>Ajouter un administrateur</h2>
         <div class="update-tab-liens">
-
+            <?= $this->Html->link(
+                "Ajouter administrateur",
+                ['controller'=> 'Users', 'action' => 'register'],
+                [
+                    'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                    'escapeTitle' => false
+                ]
+            ) ?>
             <?= $this->Html->image('administrator.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
+            <?= $this->Html->link(
+                "Supprimer administrateur",
+                ['controller'=> 'Users', 'action' => 'view'],
+                [
+                    'class' => 'text-white text-decoration-none btn btn-secondary align-self-end col-* px-2 m-3',
+                    'escapeTitle' => false
+                ]
+            ) ?>
         </div>
-    </a>
+    </div>
 
     <div class="tab text-center" >
-        <h2 class="mt-3" >Modifier des Actualité</h2>
+        <h2 class="mt-3" >Modifier des Actualités</h2>
         <div class="update-tab-liens" >
             <?= $this->Html->link(
                 "Ajouter Actualité",
@@ -42,7 +57,7 @@
                     'escapeTitle' => false
                 ]
             ) ?>
-            <?= $this->Html->image('quiz.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
+            <?= $this->Html->image('quizicon.png', ['class' => 'img-fluid h-100','alt' => 'NFT image', 'style' => 'max-height: 100%;' ]); ?>
 
             <?= $this->Html->link(
                 "Modifier Quiz",
