2f25f5520d35f403420d3afe9b63f620f33a1615
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-02-23 16:56:00 +0100

    login animation onfocus

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -19,12 +19,12 @@
                     <?= $this->Form->control('email', [
                         'required' => true
                     ]) ?>
-                    <i></i>
+                    <i id="loginI" ></i>
                 </div>
                 <div class="inputBox">
                     <?= $this->Form->control('password',
                         ['required' => true]) ?>
-                    <i></i>
+                    <i id="passwordI" ></i>
                 </div>
                 <?= $this->Form->submit(__('Login'), ['id' => 'login-btn-connexion','class'=>'grow']); ?>
                 <?= $this->Form->end() ?>
@@ -32,3 +32,29 @@
         </div>
     </div>
 </main>
+<script>
+var logInput = document.getElementById('email');
+var passInput = document.getElementById('password');
+var logi =document.getElementById('loginI');
+var passi =document.getElementById('passwordI');
+
+logInput.addEventListener('focus',function(){
+    logi.style.height = '44px';
+    logInput.style.color = 'black'
+})
+
+logInput.addEventListener('blur',function(){
+    logi.style.height = '';
+    logInput.style.color = 'white' 
+})
+
+passInput.addEventListener('focus',function(){
+    passi.style.height = '44px';
+    passInput.style.color = 'black'
+})
+
+passInput.addEventListener('blur',function(){
+    passi.style.height = '';
+    passInput.style.color = 'white' 
+})
+</script>
