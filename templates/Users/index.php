7d70311d72382a070cec740616ef2797db2268a1
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-03-07 08:51:16 +0100

    CSS delete users

--- templates/Users/index.php
+++ templates/Users/index.php
@@ -10,45 +10,25 @@
 
 ?>
 
-<div class="users index content">
-
-    <h3><?= __('Users') ?></h3>
-
-    <div class="table-responsive">
-
+<main id="usersdelete-main" class="navmarge" >
+    <h3 id="usersdelete-title" ><?= __('Users') ?></h3>
+    <div id="usersdelete-conteneur-table">
         <table>
-
-            <thead>
-
-            <tr>
-
-                <th><?= $this->Paginator->sort('id') ?></th>
-
-                <th><?= $this->Paginator->sort('name') ?></th>
-
-                <th><?= $this->Paginator->sort('email') ?></th>
-
-                <th class="actions"><?= __('Actions') ?></th>
-
-            </tr>
-
-            </thead>
-
-            <tbody>
-
-            <?php foreach ($users as $user): ?>
-
+            <thead id="usersdelete-table-thead" >
                 <tr>
-
-                    <td><?= $this->Number->format($user->id) ?></td>
-
-                    <td><?= h($user->name) ?></td>
-
-                    <td><?= h($user->email) ?></td>
-
-                    <td class="actions">
-
-
+                    <th class="usersdelete-table-th-td" ><?= $this->Paginator->sort('id') ?></th>
+                    <th class="usersdelete-table-th-td" ><?= $this->Paginator->sort('name') ?></th>
+                    <th class="usersdelete-table-th-td" ><?= $this->Paginator->sort('email') ?></th>
+                    <th class="actions usersdelete-table-th-td"><?= __('Actions') ?></th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($users as $user): ?>
+                <tr>
+                    <td class="usersdelete-table-th-td" ><?= $this->Number->format($user->id) ?></td>
+                    <td class="usersdelete-table-th-td" ><?= h($user->name) ?></td>
+                    <td class="usersdelete-table-th-td" ><?= h($user->email) ?></td>
+                    <td class="actions usersdelete-table-th-td">
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id)]) ?>
                     </td>
                 </tr>
@@ -56,19 +36,15 @@
             </tbody>
         </table>
     </div>
-
-    <div class="paginator">
-
+    <div id="usersdelete-paginator" class="paginator">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
             <?= $this->Paginator->prev('< ' . __('previous')) ?>
             <?= $this->Paginator->numbers() ?>
             <?= $this->Paginator->next(__('next') . ' >') ?>
             <?= $this->Paginator->last(__('last') . ' >>') ?>
-
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-
     </div>
+</main>
 
-</div>
