6d0af28a93328a01b984be8a0d65cbea417ade1c
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-03-07 12:18:43 +0100

    CSS INDEX ACTU, USERS, ARTICLES, QUIZ

--- templates/Quiz/index.php
+++ templates/Quiz/index.php
@@ -4,60 +4,55 @@
  * @var iterable<\App\Model\Entity\Quiz> $quiz
  */
 ?>
-<body class="bg-secondary pt-5 mt-5 " >
-<?= $this->element('nav_admin')?>
-<main class="p-5 mt-5" >
-    <div class="quiz index content bg-dark p-4 rounded-3 text-white">
-        <?= $this->Html->link(__('New Quiz'), ['action' => 'add'],  ['class' => 'btn btn-warning']) ?>
-        <h3><?= __('Quiz') ?></h3>
-        <div class="table-responsive bg-secondary rounded p-3 text-dark">
-            <table>
-                <thead>
+<main class="navmarge index-main">
+    <?= $this->Html->link(__('New Quiz'), ['action' => 'add'],  ['class' => 'index-btn-new grow']) ?>
+    <h3 class="index-title" ><?= __('Quiz') ?></h3>
+    <div class="index-conteneur-table">
+        <table>
+            <thead class="index-table-thead" >
+            <tr>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('id') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('level') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('question') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('answer1') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('answer2') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('answer3') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('realanswer') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('questionform') ?></th>
+                <th class="index-table-th-td" ><?= $this->Paginator->sort('category') ?></th>
+                <th class="actions index-table-th-td"><?= __('Actions') ?></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($quiz as $quiz): ?>
                 <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('level') ?></th>
-                    <th><?= $this->Paginator->sort('question') ?></th>
-                    <th><?= $this->Paginator->sort('answer1') ?></th>
-                    <th><?= $this->Paginator->sort('answer2') ?></th>
-                    <th><?= $this->Paginator->sort('answer3') ?></th>
-                    <th><?= $this->Paginator->sort('realanswer') ?></th>
-                    <th><?= $this->Paginator->sort('questionform') ?></th>
-                    <th><?= $this->Paginator->sort('category') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
+                    <td class="index-table-th-td" ><?= $this->Number->format($quiz->id) ?></td>
+                    <td class="index-table-th-td" ><?= $quiz->level === null ? '' : $this->Number->format($quiz->level) ?></td>
+                    <td class="index-table-th-td" ><?= h($quiz->question) ?></td>
+                    <td class="index-table-th-td" ><?= h($quiz->answer1) ?></td>
+                    <td class="index-table-th-td" ><?= h($quiz->answer2) ?></td>
+                    <td class="index-table-th-td" ><?= h($quiz->answer3) ?></td>
+                    <td class="index-table-th-td" ><?= $quiz->realanswer === null ? '' : $this->Number->format($quiz->realanswer) ?></td>
+                    <td class="index-table-th-td" ><?= h($quiz->questionform) ?></td>
+                    <td class="index-table-th-td" ><?= h($quiz->category) ?></td>
+                    <td class="actions index-table-th-td">
+                        <?= $this->Html->link(__('View'), ['action' => 'view', $quiz->id]) ?>
+                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $quiz->id]) ?>
+                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $quiz->id], ['confirm' => __('Are you sure you want to delete # {0}?', $quiz->id)]) ?>
+                    </td>
                 </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($quiz as $quiz): ?>
-                    <tr>
-                        <td><?= $this->Number->format($quiz->id) ?></td>
-                        <td><?= $quiz->level === null ? '' : $this->Number->format($quiz->level) ?></td>
-                        <td><?= h($quiz->question) ?></td>
-                        <td><?= h($quiz->answer1) ?></td>
-                        <td><?= h($quiz->answer2) ?></td>
-                        <td><?= h($quiz->answer3) ?></td>
-                        <td><?= $quiz->realanswer === null ? '' : $this->Number->format($quiz->realanswer) ?></td>
-                        <td><?= h($quiz->questionform) ?></td>
-                        <td><?= h($quiz->category) ?></td>
-                        <td class="actions">
-                            <?= $this->Html->link(__('View'), ['action' => 'view', $quiz->id]) ?>
-                            <?= $this->Html->link(__('Edit'), ['action' => 'edit', $quiz->id]) ?>
-                            <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $quiz->id], ['confirm' => __('Are you sure you want to delete # {0}?', $quiz->id)]) ?>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-        <div class="paginator">
-            <ul class="pagination">
-                <?= $this->Paginator->first('<< ' . __('first')) ?>
-                <?= $this->Paginator->prev('< ' . __('previous')) ?>
-                <?= $this->Paginator->numbers() ?>
-                <?= $this->Paginator->next(__('next') . ' >') ?>
-                <?= $this->Paginator->last(__('last') . ' >>') ?>
-            </ul>
-            <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-        </div>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="paginator index-paginator">
+        <ul class="pagination">
+            <?= $this->Paginator->first('<< ' . __('first')) ?>
+            <?= $this->Paginator->prev('< ' . __('previous')) ?>
+            <?= $this->Paginator->numbers() ?>
+            <?= $this->Paginator->next(__('next') . ' >') ?>
+            <?= $this->Paginator->last(__('last') . ' >>') ?>
+        </ul>
+        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
 </main>
-</body>
--- templates/Users/index.php
+++ templates/Users/index.php
@@ -10,25 +10,25 @@
 
 ?>
 
-<main id="usersdelete-main" class="navmarge" >
-    <h3 id="usersdelete-title" ><?= __('Users') ?></h3>
-    <div id="usersdelete-conteneur-table">
+<main class="navmarge index-main" >
+    <h3 class="index-title" ><?= __('Users') ?></h3>
+    <div class="index-conteneur-table">
         <table>
-            <thead id="usersdelete-table-thead" >
+            <thead class="index-table-thead" >
                 <tr>
-                    <th class="usersdelete-table-th-td" ><?= $this->Paginator->sort('id') ?></th>
-                    <th class="usersdelete-table-th-td" ><?= $this->Paginator->sort('name') ?></th>
-                    <th class="usersdelete-table-th-td" ><?= $this->Paginator->sort('email') ?></th>
-                    <th class="actions usersdelete-table-th-td"><?= __('Actions') ?></th>
+                    <th class="index-table-th-td" ><?= $this->Paginator->sort('id') ?></th>
+                    <th class="index-table-th-td" ><?= $this->Paginator->sort('name') ?></th>
+                    <th class="index-table-th-td" ><?= $this->Paginator->sort('email') ?></th>
+                    <th class="actions index-table-th-td"><?= __('Actions') ?></th>
                 </tr>
             </thead>
             <tbody>
             <?php foreach ($users as $user): ?>
                 <tr>
-                    <td class="usersdelete-table-th-td" ><?= $this->Number->format($user->id) ?></td>
-                    <td class="usersdelete-table-th-td" ><?= h($user->name) ?></td>
-                    <td class="usersdelete-table-th-td" ><?= h($user->email) ?></td>
-                    <td class="actions usersdelete-table-th-td">
+                    <td class="index-table-th-td" ><?= $this->Number->format($user->id) ?></td>
+                    <td class="index-table-th-td" ><?= h($user->name) ?></td>
+                    <td class="index-table-th-td" ><?= h($user->email) ?></td>
+                    <td class="actions index-table-th-td">
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->id)]) ?>
                     </td>
                 </tr>
@@ -36,7 +36,7 @@
             </tbody>
         </table>
     </div>
-    <div id="usersdelete-paginator" class="paginator">
+    <div class="paginator index-paginator">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
             <?= $this->Paginator->prev('< ' . __('previous')) ?>
