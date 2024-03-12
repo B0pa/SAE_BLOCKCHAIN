fa4c3ea036408cb39c2538456b63aeb8903f1aea
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-03-12 16:20:07 +0100

    CSS RECHERCHE

--- templates/element/search.php
+++ templates/element/search.php
@@ -5,10 +5,10 @@ $pageTitle = $currentURL; // Valeur par défaut
 ?>
 
 
-    <form id="searchForm" action="<?= $this->Url->build(['controller' => 'Articles', 'action' => 'search', $currentURL]) ?>" method="get" class="bg-dark position-fixed slideFromRight p-4 rounded-3 border border-1 border-warning" style="top:200px; right:-2px;z-index: 1;">
-        <input type="text" name="query" placeholder="Rechercher..." class="form-control">
-        <button type="submit" class="btn btn-secondary my-2 col-12" >Rechercher</button>
-        <div class="d-flex" >
+    <form id="searchForm" action="<?= $this->Url->build(['controller' => 'Articles', 'action' => 'search', $currentURL]) ?>" method="get" class="" style="">
+        <input type="text" name="query" placeholder="Rechercher..." class="search-input-search">
+        <button type="submit" class="search-btn-search grow" >Rechercher</button>
+        <div id="search-conteneur-niv" >
             <input type="checkbox" name="levels[]" value="Niv 1" id="Niv 1" class="myformcheck form-check-input align-self-center" checked>
             <label for="Niv 1" class="myformlabel form-check-label bg-secondary p-2 rounded-3 mx-auto">Niv 1</label>
 
