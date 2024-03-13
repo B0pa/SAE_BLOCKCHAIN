273507caf1268833ab61a9c0bf82748c8c8b8f7f
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-03-13 16:17:24 +0100

    Ajout de navigation des quiz + logout en icone

--- templates/element/nav.php
+++ templates/element/nav.php
@@ -27,6 +27,15 @@ $loggedIn = $this->Authentication->isLoggedIn();
                     <li class="navbtn" ><?php echo $this->Html->link('<h4>Les Dangers</h4>', ['controller'=> 'Articles', 'action' => 'danger'], ['class'=>($currentURL === "danger" ? ' active' : ''), 'escapeTitle' => false]);?></li>
                 </ul>
             </li>
+            <li class="navbtn <?php if($currentURL==="blockchain"||$currentURL==="nft"||$currentURL==="crypto"||$currentURL==="danger") echo' active';?>" >
+                <h4 class="" >Les Quiz :</h4>
+                <ul class="sousmenu">
+                    <li class="navbtn" ><?php echo $this->Html->link('<h4>Quiz Blockchain </h4>', ['controller'=> 'Quizzes', 'action' => 'quizz_blockchain'], ['class'=>($currentURL === "quizz_blockchain" ? ' active' : ''), 'escapeTitle' => false]);?></li>
+                    <li class="navbtn" ><?php echo $this->Html->link('<h4>Quiz NFT</h4>', ['controller'=> 'Quizzes', 'action' => 'quizz_n_f_t'], ['class'=>($currentURL === "quizz_n_f_t" ? ' active' : ''), 'escapeTitle' => false]);?></li>
+                    <li class="navbtn" ><?php echo $this->Html->link('<h4>Quiz Crypto</h4>', ['controller'=> 'Quizzes', 'action' => 'quizzcrypto'], ['class'=>($currentURL === "quizzcrypto" ? ' active' : ''), 'escapeTitle' => false]);?></li>
+                    <li class="navbtn" ><?php echo $this->Html->link('<h4>Quiz Dangers</h4>', ['controller'=> 'Quizzes', 'action' => 'quizz_danger'], ['class'=>($currentURL === "quizz_danger" ? ' active' : ''), 'escapeTitle' => false]);?></li>
+                </ul>
+            </li>
             <li class="navbtn" ><?php echo $this->Html->link('<h4>Les Actu</h4>', ['controller'=> 'Actualities', 'action' => 'actuality'], ['class' => 'navbtn' . ($currentURL === "actuality" ? ' active' : ''), 'escapeTitle' => false]);?></li>
             <li class="navbtn" ><?php echo $this->Html->link('<h4>Courbes en Temps Réel</h4>', ['controller'=> 'Pages', 'action' => 'tempreel'], ['class' => 'navbtn' . ($currentURL === "tempreel" ? ' active' : ''), 'escapeTitle' => false]);?></li>
 
@@ -36,20 +45,27 @@ $loggedIn = $this->Authentication->isLoggedIn();
             if ($loggedIn):
                 ?>
                 <li class="navbtn" ><?php echo $this->Html->link('<h4>Admin</h4>', ['controller'=> 'Users', 'action' => 'update'], ['class' => 'navbtn' . ($currentURL === "users" ||$currentURL === "update" || $currentURL === "articles" || $currentURL === "actualities" || $currentURL === "quizzes" || $currentURL === "register" || $currentURL == "add" ? ' active' : ''), 'escapeTitle' => false]);?></li>
-                <li class="navbtn" ><?php echo $this->Html->link('<h4>Logout</h4>', ['controller'=> 'Users', 'action' => 'logout'], ['class' => 'navbtn' . ($currentURL === "logout" ? ' active' : ''), 'escapeTitle' => false]);?></li>
-
             <?php
             endif; ?>
         </ul>
     </nav>
+    <div id="nav-conteneur-lien" >
+        <?php
+            if ($loggedIn):
+            echo $this->Html->link(
+                $this->Html->image('logout.png', ['alt' => 'image de profil', 'class' => 'nav-profil-img']),
+                ['controller'=> 'Users', 'action' => 'logout'],
+                ['class' => 'conteneur-lien', 'escapeTitle' => false]
+            );
+            endif;
 
-
-    <?php
-    $class = ($currentURL === "profil") ? 'active' : '';
-    echo $this->Html->link(
-        $this->Html->image('profil.png', ['alt' => 'image de profil', 'id' => 'nav-profil-img', 'class' => $class]),
-        ['controller'=> 'Pages', 'action' => 'profil'],
-        ['id' => 'conteneur-lien', 'escapeTitle' => false, 'class' => $class]
-    );
-    ?>
+            $class = ($currentURL === "profil") ? 'active' : '';
+            echo $this->Html->link(
+                $this->Html->image('profil.png', ['alt' => 'image de profil','class' => 'nav-profil-img ' . $class]),
+                ['controller'=> 'Pages', 'action' => 'profil'],
+                ['escapeTitle' => false, 'class' => 'conteneur-lien ' . $class]
+            );
+        ?>
+    </div>
+    
 </header>
