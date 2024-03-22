37a28b1cf9a6000ec853a33e0a74ed0eaaed0a55
Thomas Perrin <thomas.perrin2@etu.univ-lyon1.fr>
2024-03-22 17:40:34 +0100

    TEMPS REEL CORRIGER PLUS RESPONSIVE

--- templates/Pages/tempreel.php
+++ templates/Pages/tempreel.php
@@ -6,7 +6,7 @@
         <h2>Courbe du Bitcoin</h2>
     </div>
 
-    <div id="temps-graphics-conteneur" >
+    <div id="temps-graphics-conteneur"  style="position: relative; height:600px; width:100%" >
         <select id="chartType" onchange="changeChartType()">
             <option value="line">Line</option>
             <option value="candlestick">Candlestick</option>
@@ -26,7 +26,7 @@
     // Récupération du context du canvas
     const ctx = document.getElementById('chart').getContext('2d');
 
-
+    console.log("TEST");
 
     ctx.canvas.width = 1000;
     ctx.canvas.height = 250;
@@ -40,61 +40,60 @@
     // Création du graphique
     var chart;
 
-    // Fonction pour créer le graphique
-    function createChart(chartType) {
-        // Destruction s'il existe déjà
-        if(chart) {
-            chart.destroy();
-        }
+// Fonction pour créer le graphique
+function createChart(chartType) {
+    // Destruction s'il existe déjà
+    if(chart) {
+        chart.destroy();
+    }
 
-        // Création
-        chart = new Chart(ctx, {
-            type: chartType,
-            data: {
-                datasets: [{
-                    label: 'BTCUSDT',
-                    data: [],
-                    borderColor: color,
-                }]
-
-            },
-            options: {
-                scales: {
-                    x: {
-                        type: 'time',
-                        time: {
-                            unit: 'minute'
-                        },
-                        grid: {
-                            display: true,
-                            color: 'rgba(255, 255, 255, 0.4)',
-                        },
-                        ticks:{
-                            color: 'rgba(255, 255, 255, 0.6)'
-                        }
+    // Création
+    chart = new Chart(ctx, {
+        type: chartType,
+        data: {
+            datasets: [{
+                label: 'BTCUSDT',
+                data: [],
+                borderColor: color,
+            }]
+        },
+        options: {
+            responsive: true, // Ajoutez cette ligne
+            maintainAspectRatio: false, // Ajoutez cette ligne
+            scales: {
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'minute'
                     },
-                    y: {
-                        grid: {
-                            display: true,
-                            color: 'rgba(255, 255, 255, 0.4)',
-                        },
-                        ticks:{
-                            color: 'rgba(255, 255, 255, 0.6)'
-                        }
+                    grid: {
+                        display: true,
+                        color: 'rgba(255, 255, 255, 0.4)',
+                    },
+                    ticks:{
+                        color: 'rgba(255, 255, 255, 0.6)'
                     }
-
                 },
-                plugins: {
-                    legend:{
-                        labels:{
-                            color:'rgba(255, 255, 255, 0.6)'
-                        }
+                y: {
+                    grid: {
+                        display: true,
+                        color: 'rgba(255, 255, 255, 0.4)',
+                    },
+                    ticks:{
+                        color: 'rgba(255, 255, 255, 0.6)'
+                    }
+                }
+            },
+            plugins: {
+                legend:{
+                    labels:{
+                        color:'rgba(255, 255, 255, 0.6)'
                     }
                 }
             }
-
-        });
-    }
+        }
+    });
+}
 
     // Formatter pour les données en mode Line
     function formatLine(candle) {
@@ -127,7 +126,12 @@
         // Formatage
         let formattedData = [];
         if(chart.config.type === 'line') {
-            formattedData = rawData.map(formatLine);
+            if(isMobileDevice()){
+                formattedData = rawData.filter((_, index) => index % 25 === 0).map(formatLine);
+            }
+            else{
+                formattedData = rawData.filter((_, index) => index % 5 === 0).map(formatLine);
+            }
         } else {
             // Take only every 10th data point for candlestick chart
             formattedData = rawData.filter((_, index) => index % 5 === 0).map(formatBar);
@@ -155,6 +159,7 @@
 
     function isMobileDevice() {
         if (
+            
             navigator.userAgent.match(/iPhone/i) ||
             navigator.userAgent.match(/webOS/i) ||
             navigator.userAgent.match(/Android/i) ||
@@ -162,9 +167,12 @@
             navigator.userAgent.match(/iPod/i) ||
             navigator.userAgent.match(/BlackBerry/i) ||
             navigator.userAgent.match(/Windows Phone/i)
-        ) {
+        ) { 
+            console.log("mobile");
             return true;
+           
         } else {
+            console.log("ordi");
             return false;
         }
     }
