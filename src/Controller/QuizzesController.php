9772e3218b5403c673bda555c8545df86eac3a20
ThoDev13 <thomas.domenech@etu.univ-lyon1.fr>
2024-04-06 20:03:35 +0200

    calcule score

--- src/Controller/QuizzesController.php
+++ src/Controller/QuizzesController.php
@@ -1,35 +1,20 @@
 <?php
-
 declare(strict_types=1);
-
-
 
 namespace App\Controller;
 
-
-
 use Cake\View\CellRegistry;
-
 use Cake\Utility\Text;
-
 use Laminas\Diactoros\UploadedFile;
-
 use Cake\Http\Cookie\Cookie;
-
 use Cake\Log\Log;
 
 
-
 /**
-
  * Quizzes Controller
-
  *
-
  * @property \App\Model\Table\QuizzesTable $Quizzes
-
  */
-
 class QuizzesController extends AppController
 
 {
@@ -40,15 +25,10 @@ class QuizzesController extends AppController
     }
 
     /**
-
      * Index method
-
      *
-
      * @return \Cake\Http\Response|null|void Renders view
-
      */
-
     public function index()
     {
         $query = $this->Quizzes->find();
@@ -59,162 +39,92 @@ class QuizzesController extends AppController
 
 
     /**
-
      * View method
-
-     *
-
      * @param string|null $id Quiz id.
-
      * @return \Cake\Http\Response|null|void Renders view
-
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-
      */
-
     public function view($id = null)
     {
         $quiz = $this->Quizzes->get($id, contain: []);
         $this->set(compact('quiz'));
     }
 
-
-
     /**
      * Add method
      *
      * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
      */
-
     public function add()
-
     {
-
         $quiz = $this->Quizzes->newEmptyEntity();
-
         if ($this->request->is('post')) {
-
             $data = $this->request->getData();
-
             // Créer un tableau pour stocker les données des réponses
-
             $answersData = [];
-
             $answerNumber = 1; // Ajoutez un compteur ici
-
             foreach ($data as $key => $value) {
-
                 if (strpos($key, 'answer') === 0) {
-
                     if ($data['questionform'] === 'image') {
-
                         /** @var UploadedFile $file */
-
                         $file = $value;
-
                         if ($file->getError() === 0 && str_contains($file->getClientMediaType(), 'image')) {
-
                             $newName = strtolower(Text::slug($file->getClientFilename(), ['preserve' => '.']));
-
                             $file->moveTo(WWW_ROOT . 'img/upload/' . $newName);
-
                             $value = $newName;
-
                         }
-
                     }
-
                     $answersData[] = ['answer' => $value, 'num' => $answerNumber]; // Ajoutez le numéro de la réponse ici
-
                     $answerNumber++; // Incrémentez le compteur après chaque réponse
-
                 }
-
             }
-
-
 
             // Ajouter les données des réponses à l'entité quiz
-
             $data['answers'] = $answersData;
 
-
-
             // dd($data);
-
             $quiz = $this->Quizzes->newEntity($data, [
-
                 'associated' => ['Answers']
-
             ]);
-
             // dd($quiz);
 
-
-
             if ($this->Quizzes->save($quiz, ['associated' => ['Answers']])) {
-
                 // dd($quiz);
-
                 // La quiz et les réponses ont été sauvegardées
-
                 $this->Flash->success(__('The quiz has been saved.'));
-
                 return $this->redirect(['action' => 'index']);
-
             } else {
-
                 // La sauvegarde a échoué
-
                 debug($quiz->getErrors());
-
             }
-
             $this->Flash->error(__('The quiz could not be saved. Please, try again.'));
-
         }
-
         $this->set(compact('quiz'));
-
     }
 
 
 
     /**
-
      * Edit method
-
      *
-
      * @param string|null $id Quiz id.
-
      * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-
      */
-
     public function edit($id = null)
-
     {
-
         $quiz = $this->Quizzes->get($id, [
             'contain' => ['Answers']
         ]);
-        
         if ($this->request->is(['patch', 'post', 'put'])) {
             $quiz = $this->Quizzes->patchEntity($quiz, $this->request->getData());
-        
             if ($this->Quizzes->save($quiz)) {
                 $this->Flash->success(__('The quiz has been saved.'));
                 return $this->redirect(['action' => 'index']);
             }
-        
             $this->Flash->error(__('The quiz could not be saved. Please, try again.'));
         }
-        
         $this->set(compact('quiz'));
-
     }
 
     /**
@@ -391,6 +301,12 @@ class QuizzesController extends AppController
         $quizzes = $this->Quizzes->find()->toArray();
         $score = 0;
         // Calculer le score et le stoocker dans un cookie                            TODOS
+        foreach ($quizzes as $key => $quiz) {
+            if ($quiz->realAnswer == $selectedAnswers[$key]) {
+                $score++;
+            }
+        }
+        $score = $score / count($quizzes) * 10000;
         $session->write('count', 0);
         $session->write('currentURL', 'defaultURL');
         $session->write('selectedAnswers', []); 
@@ -419,104 +335,55 @@ class QuizzesController extends AppController
             $validation_cookie = Cookie::create(
                 'validation',
                 1,
-
                 // All keys are optional
-
                 [
-
                     'expires' => new \DateTime('+1 day'),
-
                     'path' => '/',
-
                     'domain' => '',
-
                     'secure' => false,
-
                     'httponly' => false,
-
                     'samesite' => null // Or one of CookieInterface::SAMESITE_* constants
-
                 ]
-
             );
-
             $this->response = $this->response->withCookie($validation_cookie);
-
         }
-
         return $this->redirect($this->referer());
-
     }
 
 
-
     public function cookieRefuse() {
-
         $cookie = $this->request->getCookie('validation');
-
         if ($cookie == null) {
-
             $validation_cookie = Cookie::create(
-
                 'validation',
-
                 2,
-
                 // All keys are optional
-
                 [
-
                     'expires' => new \DateTime('+1 day'),
-
                     'path' => '/',
-
                     'domain' => '',
-
                     'secure' => false,
-
                     'httponly' => false,
-
                     'samesite' => null // Or one of CookieInterface::SAMESITE_* constants
-
                 ]
-
             );
-
             $this->response = $this->response->withCookie($validation_cookie);
-
         } else {
-
             $validation_cookie = Cookie::create(
-
                 'validation',
-
                 2,
-
                 // All keys are optional
-
                 [
-
                     'expires' => new \DateTime('+1 day'),
-
                     'path' => '/',
-
                     'domain' => '',
-
                     'secure' => false,
-
                     'httponly' => false,
-
                     'samesite' => null // Or one of CookieInterface::SAMESITE_* constants
-
                 ]
-
             );
-
             $this->response = $this->response->withCookie($validation_cookie);
-
         }
-
         return $this->redirect($this->referer());
-
     }
 }
